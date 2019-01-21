762950606dd5120efabe0725a3078d5bddac9846
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-01-21 12:59:47 +0000

    Favourite language

--- specs/students_spec.rb
+++ specs/students_spec.rb
@@ -15,4 +15,17 @@ class TestCodeclanStudents < MiniTest::Test
   def test_cohort
     assert_equal('E28', @account.cohort)
   end
+
+  def test_set_name
+    @account.name = "Anna"
+    assert_equal('Anna', @account.name)
+  end
+
+  def test_get_talk
+    assert_equal('I can talk!', @account.get_talk())
+  end
+
+  def test_favourite_language
+    assert_equal('I love Ruby', @account.favourite_language('Ruby'))
+  end
 end
--- students.rb
+++ students.rb
@@ -15,5 +15,16 @@ def get_cohort
   return @cohort
 end
 
+def set_name(new_name)
+  return @name = new_name
+end
 
+def get_talk
+  return "I can talk!"
+end
+
+def favourite_language(language)
+  return "I love #{language}"
+
+end
 end
